da773a07b040b871cca1f36eb0ba63d6db93ce40
H.Lee <lixionghui2015@gmail.com>
2020-07-19 12:45:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,3 +13,4 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+svn co https://github.com/pymumu/openwrt-smartdns /feeds/packages/net/smartdns/
